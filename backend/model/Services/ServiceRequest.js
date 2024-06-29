6afe705d75af55152db3a8af0eb245aa0193659e
Jainil <jainilprajapati8095@gmail.com>
2024-06-29 11:13:40 +0530

    initial login commit

---
+++ backend/model/Services/ServiceRequest.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const serviceRequestSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    serviceProvider: { type: Schema.Types.ObjectId, ref: 'User' },
+    location: {
+        coordinates: { type: [Number], required: true } // [longitude, latitude]
+    },
+    rating: { type: Number, min: 0, max: 5 },
+    typeOfAssistance: { type: String, required: true },
+    status: { type: String, enum: ['Pending', 'Accepted', 'In Progress', 'Completed', 'Cancelled'], default: 'Pending' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+serviceRequestSchema.index({ location: '2dsphere' });
+
+const ServiceRequest = mongoose.model('ServiceRequest', serviceRequestSchema);
+
+module.exports = ServiceRequest;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,98 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Index World');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const connectDB = require('./model/dbconnection');
+
+const User = require('./model/User');
+
+// Load environment variables
+require('dotenv').config();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware for body-parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true })); 
+
+// Middleware for JWT
+app.use((req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        res.status(401);
+        res.json({ status: 'error', error: 'login required! Please login' });
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400);
+        if( error.name === 'TokenExpiredError' ){
+            res.json({ status: 'error', error: 'Invalid Token or expired' });
+        }
+        else{
+            res.json({ status: 'error', error: 'Invalid Token' });
+        }
+    }
 });
 
-app.get('/api', (req, res) => {
-    res.send('Hello World');
+
+// Routes
+
+// user login 
+app.post('/login', (req, res) => {
+
+
 });
 
+// user registration
+app.post('/register', (req, res) => {
+    let {
+        name,
+        email,
+        mobile_no,
+        password,
+        role,
+        loc = null
+    } = req.body;
+    
+    // check if user already exists
+    const user = User;
+    user.findOne({ email: email }, (err, user) => {
+        if (err) {
+            res.status(500);
+            res.json({ status: 'error', error: 'Internal error please try again' });
+        } else if (user) {
+            res.status(400);
+            res.json({ status: 'error', error: 'User already exists' });
+        } else {
+            const newUser = new User({
+                name,
+                email,
+                mobile_no,
+                password,
+                role,
+                loc
+            });
+
+            newUser.save((err, user) => {
+                if (err) {
+                    res.status(500);
+                    res.json({ status: 'error', error: 'Internal error please try again' });
+                } else {
+                    res.status(200);
+                    res.json({ status:'success', message: 'User created successfully' });
+                }
+            });
+        }
+    });
+
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
